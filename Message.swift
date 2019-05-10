db00774b0776ad2a1e297632dea9ead32b064291
Chris Houston <chris_houston@intuit.com>
2019-05-10 12:04:55 -0600

    first commit

---
+++ Message.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Vapor
+import FluentSQLite
+
+struct Message: Content, SQLiteUUIDModel, Migration {
+    var id: UUID?
+    var username: String
+    var content: String
+    var date: Date
+}
